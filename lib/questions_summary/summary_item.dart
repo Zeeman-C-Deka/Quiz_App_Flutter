62b2986634fc954b3b392fb0c3a467577a184a07
Zeeman-C-Deka <79737506+Zeeman-C-Deka@users.noreply.github.com>
2023-06-16 23:56:00 +0530

    Update summary_item.dart

--- lib/questions_summary/summary_item.dart
+++ lib/questions_summary/summary_item.dart
@@ -18,7 +18,7 @@ class SummaryItem extends StatelessWidget {
       children: [
         QuestionIdentifier(
           isCorrectAnswer: isCorrectAnswer,
-          questionIndex: itemData['question'] as int,
+          questionIndex: itemData['question_index'] as int,
         ),
         const SizedBox(width: 20),
         Expanded(
